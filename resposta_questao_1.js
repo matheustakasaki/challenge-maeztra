39c0f297161179b4be430a183eefff4dbd049809
matheustakasaki <matheus8antunes@gmail.com>
2022-10-19 21:23:56 -0300

    v1 da primeira questão

--- resposta_questao_1.js
+++ resposta_questao_1.js
@@ -2,14 +2,14 @@
 // verifica se os valores estão em ordenação sequencial crescente ou decrescente
 
 
-const numberTest = 152456457;
+
 
 const handleIsPositiveInteger = (number) => {
     if (number % 1 === 0 && Math.sign(number) === 1) {
-        console.log('É inteiro positivo')
+
         return true
     } else if (number % 1 === 0 && Math.sign(number) === -1) {
-        console.log('É Inteiro negativo')
+
         return false
     } else {
         console.log('É float')
@@ -18,22 +18,45 @@ const handleIsPositiveInteger = (number) => {
     }
 }
 
-const handleIsOrdenated = (number) => {
-
-    // Se inteiro positivo = true
-    const nTeste = handleIsPositiveInteger(number)
-
-    // Converto para string
-    const sequence = number.toString();
-    const sequenceArr = Array.from(sequence)
-    function compararNumeros(a, b) {
-        return a - b;
-    }
-
-    sequenceArr.forEach((element, index, array) => {
-        console.log(element)
+function numberToArray(number) {
+    let arr = String(number).split("").map((num) => {
+        return Number(num)
     })
+    console.log('Array: \n', arr, '\n')
+    return arr
+}
+const numberTest = 1786;
+const handleIsOrdenated = (number) => {
+    // Se inteiro positivo = true
+    let valores = numberToArray(number)
 
+    // Para cada item no array de valores
+    let testSequence = []
+
+    valores.forEach((element, index, lista) => {
+        // console.log(element, index)
+
+        testSequence.push(element === lista[index + 1] || element === lista[index + 1] - 1 || element === lista[index + 1] + 1)
+
+    });
+
+    testSequence.pop()
+
+    var valoresUnicos = testSequence.filter(function (elem, index, self) {
+        return index === self.indexOf(elem);
+    });
+
+    console.log(valoresUnicos)
+
+    if (valoresUnicos) {
+        console.log('Está ordenado')
+        return 'Está ordenado'
+    }
+    else {
+
+        console.log('Não está ordenado')
+        return 'Não está ordenado'
+    }
 }
 
 handleIsOrdenated(numberTest)
--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -1,1 +1,14 @@
 
+const numeros = [4, 5, 44, 98, 4, 5, 6, 7]
+
+const handleCheckClone = (numeros) => {
+    numeros.filter(function (element, i) {
+        return numeros.indexOf(element) === i
+    })
+}
+
+const novArr = numeros.filter(function (e, i) {
+    return numeros.indexOf(e) === i
+})
+
+console.log(novArr)
